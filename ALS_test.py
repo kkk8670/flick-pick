fb4aaa424e4f22bb900c32154c27fdb4d6230b36
chloe <chloe0088l@gmail.com>
2025-03-19 09:23:25 +0800

    feat:kafka

--- ALS_test.py
+++ ALS_test.py
@@ -1,18 +1,24 @@
 from pyspark.sql import SparkSession
 from pyspark.ml.recommendation import ALS
 from pyspark.sql.functions import col
+from pyspark.sql import functions as F
+
+import os
+os.environ['JAVA_HOME'] = '/opt/homebrew/opt/openjdk@17'
 
 # file_path = "dbfs:/FileStore/tables/test/ratings.csv"
-file_path = "./data/ml-latest-small/ratings.csv"
+file_path = "./data/test/ratings.csv"
 
 
 # init
-spark = SparkSession.builder.appName("flick-pick").getOrCreate()
+spark = SparkSession \
+    .builder \
+    .appName("flick-pick") \
+    .config("spark.driver.bindAddress", "localhost") \
+    .getOrCreate() 
 
- 
 ratings = spark.read.csv(file_path, header=True, inferSchema=True)
 
- 
 ratings = ratings.select(col("userId"), col("movieId"), col("rating"))
 
 # training
@@ -32,12 +38,28 @@ model = als.fit(ratings)
 # recommend for all users
 recommendations = model.recommendForAllUsers(10) 
 
-# recommend for userid=5
-user_id = 6
-# user_df = spark.createDataFrame([(user_id,)], ["userId"])
-# recommendations = model.recommendForUserSubset(user_df, numItems=10)
+def recommend_to(user_id):
+    # user_df = spark.createDataFrame([(user_id,)], ["userId"])
+    # recommendations = model.recommendForUserSubset(user_df, numItems=10)
 
-recommendations = recommendations.filter(col("userId") == user_id) 
-recommendations.show(5, truncate=False)  # show 前五条
+    recommendations = recommendations.filter(col("userId") == user_id) 
+    recommendations.show(5, truncate=False)  # show 前五条
+
+
+
+def flat_data(recommendations):
+    flat_recommendations = recommendations.withColumn("rec", F.explode("recommendations"))
+    flat_recommendations = flat_recommendations.select(
+        "userId", 
+        F.col("rec.movieId").alias("movieId"), 
+        F.col("rec.rating").alias("rating")
+    )
+
+    # save
+    pd = flat_recommendations.toPandas()
+    pd.to_csv('./data/for_visual_sample.csv', index=True, header=True)
+
+# recommend_to()
+flat_data(recommendations)
 
 spark.stop()
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # flick-pick
+
+
+- 如果python >= v3.12， 报错`No module named 'distutils'`，使用命令`pip install -U pip setuptools wheel`
+- 本地spark需要配置openjdk@17
